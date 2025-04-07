257bc1a49950fe58c4044383e99f9c7682985334
Madalina Zurini <madalina.zurini@csie.ase.ro>
2025-04-07 04:41:39 +0300

    C07

---
+++ Curs/DP/src/dp/C7/composite/clase/Meniu.java
@@ -0,0 +1,22 @@
+package dp.C7.composite.clase;
+
+
+public class Meniu{
+
+	Structura structura;
+	String numeRestaurant;
+	
+	public Meniu(Structura structura, String numeRestaurant){
+		this.structura = structura;
+		this.numeRestaurant = numeRestaurant;
+	}
+
+	public String getNumeRestaurant() {
+		return numeRestaurant;
+	}
+
+	public void setNumeRestaurant(String numeRestaurant) {
+		this.numeRestaurant = numeRestaurant;
+	}
+
+}
---
+++ Curs/DP/src/dp/C7/proxy/clase/IPetrecere.java
@@ -0,0 +1,6 @@
+package dp.C7.proxy.clase;
+
+public interface IPetrecere {
+	public void adaugaParticipant(Client cl);
+	public void afisareProgram();
+}
