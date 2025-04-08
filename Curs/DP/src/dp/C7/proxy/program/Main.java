282734eb8e5465a3de485caa9759b56b75a1d62f
Madalina Zurini <madalina.zurini@csie.ase.ro>
2025-04-08 12:41:50 +0300

    C07

--- Curs/DP/src/dp/C7/proxy/program/Main.java
+++ Curs/DP/src/dp/C7/proxy/program/Main.java
@@ -1,6 +1,7 @@
 package dp.C7.proxy.program;
 
 import dp.C7.proxy.clase.Client;
+import dp.C7.proxy.clase.IPetrecere;
 import dp.C7.proxy.clase.Petrecere;
 import dp.C7.proxy.clase.PetrecereProxy;
 
@@ -9,6 +10,7 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
+        //inainte de restrictii prin proxy
         List<String> listaInvitati = new ArrayList<>();
         listaInvitati.add("Mos Craciun");
         listaInvitati.add("Renul Rudolf");
@@ -16,12 +18,14 @@ public class Main {
         Client cl1 = new Client("Gigel", 20);
         Client cl2 = new Client("Costel", 12);
         Petrecere petrecere = new Petrecere("24.12.2022", listaInvitati);
-        petrecere.adaugaParticipant(cl1);
+        petrecere.adaugaParticipant(cl2);
 
+        System.out.println("Afisare dupa proxy");
         Client cl3 = new Client("Marcel", 20);
         Client cl4 = new Client("Ionela", 12);
-        PetrecereProxy petrecereProxy = new PetrecereProxy(petrecere);
-        petrecereProxy.adaugaParticipant(cl3);
+        IPetrecere petrecereProxy = new PetrecereProxy(petrecere);
+        petrecere.adaugaParticipant(cl3);
+        //petrecereProxy.adaugaParticipant(cl3);
         petrecereProxy.adaugaParticipant(cl4);
     }
 }
